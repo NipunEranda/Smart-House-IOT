a29ff90278d766a830dcb07c2b3a497f9043b2ef
IT18009446 <it18009446@my.sliit.lk>
2021-07-22 20:18:06 +0530

    script updated

--- client.py
+++ client.py
@@ -2,23 +2,20 @@
 
 import paho.mqtt.client as mqtt
 
+host = "5.196.95.208"
 def on_connect(client, userdata, flags, rc):
 	print("Connected with result code " + str(rc))
-	client.subscribe("coreElectronics/test")
-	client.subscribe("coreElectronics/topic")
+	client.subscribe("iotSmartHouse001/lightDecision")
 
 def on_message(client, userdata, msg):
-	print(msg.topic + " " + str(msg.payload))
-
-	if msg.payload == "Hello":
-		print("Received message #1, Do something")
-
-	if msg.payload == "world":
-		print("Received message #2, Do something")
+	value = str(msg.payload).replace("'", "").replace("b", "")
+	print("")
+	print("Value : " + value.split(",")[0])
+	print("Decision : " + value.split(",")[1])
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("test.mosquitto.org", 1883, 60)
+client.connect(host, 1883, 60)
 client.loop_forever()
--- server.py
+++ server.py
@@ -1,7 +1,17 @@
 #MQTT Publish
+from random import randint
 
 import paho.mqtt.publish as publish
+
+host = "5.196.95.208"
 while True:
-	publish.single("coreElectronics/test", "Hello", hostname="test.mosquitto.org")
-	publish.single("coreElectronics/topic", "world", hostname="test.mosquitto.org")
-print("Done")
+	x = randint(0, 2000)
+	if(x > 0 and x < 500):
+		publish.single("iotSmartHouse001/lightDecision", str(x) + ",Low", hostname=host)
+	elif(x > 500 and x < 1000):
+		publish.single("iotSmartHouse001/lightDecision", str(x) + ",Medium", hostname=host)
+	elif(x > 1000):
+		publish.single("iotSmartHouse001/lightDecision", str(x) + ",High", hostname=host)
+	else:
+		publish.single("iotSmartHouse001/lightDecision", "Error", hostname=host)
+	
