48d8283f6e05a9e86533431badafefd9e70b8cba
IT18009446 <it18009446@my.sliit.lk>
2021-08-12 15:55:42 +0530

    backend server updated

--- webapp/backend/main.py
+++ webapp/backend/main.py
@@ -3,6 +3,8 @@ from flask_restful import Resource, Api
 from flask_cors import CORS, cross_origin
 import RPi.GPIO as GPIO
 import time
+import csv
+import os
 
 GPIO.setmode(GPIO.BOARD)
 delayT = .1
@@ -14,10 +16,44 @@ GPIO.setwarnings(False)
 GPIO.setup(led, GPIO.OUT)
 GPIO.output(led, False)
 
+def rc_time(ldr):
+        count = 0
+
+        GPIO.setup(ldr, GPIO.OUT)
+        GPIO.output(ldr, False)
+        time.sleep(delayT)
+
+        GPIO.setup(ldr, GPIO.IN)
+
+        while(GPIO.input(ldr) == 0):
+                count += 1
+
+        return count
+        
+def initialDirCreator():
+	homedir = os.environ['HOME']
+	if(os.path.isfile(homedir + "/scheduler.csv")):
+		f = open(homedir + '/scheduler.csv', 'r')
+		reader = csv.reader(f)
+		lineCount= len(list(reader))
+		if(lineCount < 1):
+			f = open(homedir + '/scheduler.csv', 'w')
+			writer = csv.writer(f)
+			writer.writerow('id','day','time','status')
+			f.close()
+	else:
+		f = open(homedir + '/scheduler.csv', 'w')
+		writer = csv.writer(f)
+		writer.writerow(['id','day','time','status'])
+		f.close()
+
+
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 api = Api(app)
+
+initialDirCreator();
 
 @app.route('/led/on', methods=['GET'], strict_slashes=False)
 @cross_origin()
@@ -40,6 +76,11 @@ def manual():
 @cross_origin()
 def auto():
         return 'AUTOMATION ENABLED'
+
+@app.route('/data/current', methods=['GET'], strict_slashes=False)
+@cross_origin()
+def getData():
+        return str(rc_time(ldr))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
