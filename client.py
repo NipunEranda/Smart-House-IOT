42f84ab85b32667e5a6b9dd6ed7b38f133b36298
NipunEranda <namarasekara71@gmail.com>
2021-10-21 17:28:03 +0530

    changes added

--- client.py
+++ client.py
@@ -14,8 +14,8 @@ def on_message(client, userdata, msg):
 	f = open("mod", "w")
 	f.write(value)
 	f.close()
-	time.sleep(1000)
 	print(value)
+	time.sleep(1000)
 
 client = mqtt.Client()
 client.on_connect = on_connect
