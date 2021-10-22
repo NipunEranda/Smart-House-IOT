05e68930db5cfd9607361a74b0d019d5b4833521
NipunEranda <namarasekara71@gmail.com>
2021-10-22 09:53:29 +0530

    bug fix

--- client.py
+++ client.py
@@ -10,10 +10,13 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
 	value = str(msg.payload).replace("'", "").replace("b", "")
-	f = open("mod", "w")
-	f.write(value)
-	f.close()
-	print(value)
+	f = open("mod", "r")
+	if(not f.read() == "value"):
+		f.close()
+		f = open("mod", "w")
+		f.write(value)
+		f.close()
+		print(value)
 
 client = mqtt.Client()
 client.on_connect = on_connect
